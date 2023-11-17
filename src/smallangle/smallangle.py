9febff24a5c1781d00eec5349e399d66d1807676
nautvanderpoel <135599215+nautvanderpoel@users.noreply.github.com>
2023-11-17 14:37:16 +0100

    I've added a group_command and option number

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,14 +3,26 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+)
+
+@cmd_group.command()
+@click.argument("number")
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
-    return
+    return 
 
-
+@cmd_group.command()
+@click.argument("number")
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +31,7 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
+
+
+
